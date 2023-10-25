801de9be3b55ef66479e4111d97a1dc411abd8e9
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-10-25 20:06:03 +0100

    [ADJUST] Update code and layout
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Fetch data from JSON file
+document.addEventListener("DOMContentLoaded", () => loadData());
+
+function loadData() {
   fetch("data.json")
     .then((response) => response.json())
-    .then((data) => {
-      // Atualizar o perfil
-      document.querySelector(".profile-name").textContent = data.profile.name;
-      document.querySelector(".job-title").textContent = data.profile.jobTitle;
-      document.querySelector(".company").textContent = data.profile.company;
-
-      // Atualizar o email
-      document.querySelector(".email").textContent = data.email;
-      document.querySelector(".email-link").href = "mailto:" + data.email;
-
-      // Atualizar o telefone
-      document.querySelector(".phone").textContent = data.phone;
-      document.querySelector(".phone-link").href = "tel:" + data.phone;
-
-      // Atualizar o website
-      document.querySelector(".website").textContent = data.website;
-      document.querySelector(".website-link").href = data.website;
-
-      // Atualizar o endereço
-      document.querySelector(".address").textContent = data.address;
+    .then(({ profile, publicFields }) => {
+      // Update the profile
+      updateHeader(profile);
+      // Add Sections
+      addSectionsToDOM(publicFields);
     })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
+    .catch((error) => console.error("Error:", error));
+}
+
+function updateHeader(profile) {
+  let classList = [
+    "w-full",
+    "h-32",
+    "bg-gradient-to-r",
+    `from-[${profile.colorFrom}]`,
+    `to-[${profile.colorTo}]`,
+  ];
+
+  document.querySelector("header").classList.add(...classList);
+  document.querySelector(".profile-picture").src = profile.picture;
+  document.querySelector(".profile-name").textContent = profile.name;
+  document.querySelector(".job-title").textContent = profile.jobTitle;
+  document.querySelector(".company").textContent = profile.company;
+}
+
+function createSection(data) {
+  return `<section class="bg-white">
+      <div class="mx-auto h-full flex items-center justify-between">
+        <a class="${data.type}-link flex justify-between" 
+          href="${data.url || "#"}">
+          <div class="w-7 h-7 bg-purple-600 rounded-full flex items-center justify-center">
+            <span class="text-white text-sm">
+              <i class="fa-solid ${data.icon}"></i>
+            </span>
+          </div>
+          <p class="text-sm text-gray-600 
+            ${data.type} ml-5 flex items-center justify-center">
+            ${data.text}
+          </p>
+        </a>
+        <div class="w-1/12 flex items-center justify-center">
+          <i class="fa-regular fa-copy"></i>
+        </div>
+      </div>
+    </section>`;
+}
+
+function addSectionsToDOM(sectionsData) {
+  const main = document.querySelector("main");
+  sectionsData.forEach((data) => {
+    main.insertAdjacentHTML("beforeend", createSection(data));
+  });
+}
